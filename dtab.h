bce2359053b9fedbd531dfc7d40bb37272da473a
Gabriel Taillon <ga.taillon@gmail.com>
2021-08-08 00:19:30 +0900

    making dtab.

--- dtab.h
+++ dtab.h
@@ -15,7 +15,8 @@ extern uint64_t dtab_hash_sdbm(const char * str);
 /********************** DTAB: DYNAMIC HASH TABLE FOR C99 v0.1 ******************/
 #define DTAB_LEN_INIT 16
 
-// DTAB_INIT: a dtab is two arrays: list of name hashes (keys) and list of values.
+// DTAB_INIT: a dtab is a struct with two arrays: list of name hashes (keys) and list of values.
+// they have len total allocated length and num active elements.
 struct dtab {
     dtab_hash_t * keys;
     void * values;
@@ -29,6 +30,15 @@ dtab_ptr->num = 0;\
 dtab_ptr->values = calloc((DTAB_LEN_INIT), sizeof(type));\
 dtab_ptr->keys = malloc(sizeof(*dtab_ptr->keys)*(DTAB_LEN_INIT));\
 
+#define DTAB_ADD(dtab_ptr, name, value) dtab_ptr->keys[dtab_ptr->num] = DTAB_HASH(name);\
+dtab_ptr->values[dtab_ptr->num]
+#define DTAB_GET(dtab_ptr, name) 
+#define DTAB_FREE(dtab_ptr) 
+#define DTAB_DEL(dtab_ptr, name) 
+#define DTAB_GROW(dtab_ptr) 
+#define DTAB_REALLOC(dtab_ptr) 
+#define DTAB_LEN(dtab_ptr) dtab_ptr->len 
+#define DTAB_NUM(dtab_ptr) dtab_ptr->num
 
 
 #endif /* DARR */
